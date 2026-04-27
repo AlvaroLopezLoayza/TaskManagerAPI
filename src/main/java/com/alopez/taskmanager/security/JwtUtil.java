f6f0e967b0934ddcb4ac4fa0dc3c548a6b60f164
Alvaro Lopez <alopez@fruchincha.com.pe>
2026-04-27 09:26:03 -0500

    fix: secret fija desde environment variables

--- src/main/java/com/alopez/taskmanager/security/JwtUtil.java
+++ src/main/java/com/alopez/taskmanager/security/JwtUtil.java
@@ -1,23 +1,26 @@
 package com.alopez.taskmanager.security;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 
 import java.security.Key;
 import java.util.Date;
 
 public class JwtUtil {
-    private static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    private static final String SECRET = System.getenv("JWT_SECRET");
+
+    private static final Key key = Keys.hmacShaKeyFor(SECRET.getBytes());
 
     public static String generateToken(String username) {
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 86400000)) // 1 día
+                .setExpiration(new Date(System.currentTimeMillis() + 86400000))
                 .signWith(key)
                 .compact();
     }
+
     public static String validateToken(String token) {
         return Jwts.parserBuilder()
                 .setSigningKey(key)
